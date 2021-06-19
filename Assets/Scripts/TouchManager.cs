8768e5206ba88b6f19d426c54da3d4f4566a281d
masakisaki <masaki.kubokawa@gmail.com>
2021-06-19 10:37:23 +0900

    secont commit

---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,44 @@
+﻿#if UNITY_EDITOR
+#define IS_EDITOR
+#endif
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TouchManager : MonoBehaviour
+{
+    //singleton
+    private static TouchManager _instance;
+
+    public static TouchManager Instance
+    {
+        get
+        {
+            if (null == _instance)
+            {
+                var obj = new GameObject(typeof(TouchManager).Name);
+                _instance = obj.AddComponent<TouchManager>();
+                if (null == _instance)
+                {
+                    Debug.Log(" TouchManager Instance Error ");
+                }
+            }
+            return _instance;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
